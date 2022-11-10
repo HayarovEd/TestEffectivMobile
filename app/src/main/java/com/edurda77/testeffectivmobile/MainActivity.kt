6f6f1ba8168252ec432ad0ab74e8f4864760bd1f
hayarovy <edurd@rambler.ru>
2022-11-10 17:16:49 +0500

    release switch data in product fragment

--- app/src/main/java/com/edurda77/testeffectivmobile/MainActivity.kt
+++ app/src/main/java/com/edurda77/testeffectivmobile/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.edurda77.testeffectivmobile
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.edurda77.mylibrary.navigation.AppNavigation
 import com.edurda77.testeffectivmobile.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navView: BottomNavigationView
+    @Inject
+    lateinit var coordinator: AppNavigation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        navView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
@@ -34,5 +40,29 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
         supportActionBar?.hide()
+        coordinator.navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navigation_home -> {
+                    showBottomNav()
+                }
+                R.id.navigation_dashboard -> {
+                    showBottomNav()
+                }
+                R.id.navigation_notifications -> {
+                    showBottomNav()
+                }
+                else -> {
+                    hideBottomNav()
+                }
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        navView.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        navView.visibility = View.GONE
     }
 }
--- productDetails/src/main/java/com/edurda77/productdetails/ui/ProductFragment.kt
+++ productDetails/src/main/java/com/edurda77/productdetails/ui/ProductFragment.kt
@@ -1,20 +1,57 @@
 package com.edurda77.productdetails.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.edurda77.productdetails.R
+import androidx.fragment.app.Fragment
+import com.edurda77.mylibrary.navigation.AppNavigation
+import com.edurda77.productdetails.databinding.FragmentProductBinding
+import com.edurda77.productdetails.utils.ProductData
+import com.edurda77.productdetails.utils.StateDataFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ProductFragment : Fragment() {
+    private var _binding: FragmentProductBinding? = null
+    private var productData: ProductData = ProductData.Shop
+    private val binding get() = _binding!!
+
+    @Inject
+    lateinit var coordinator: AppNavigation
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        return inflater.inflate(R.layout.fragment_product, container, false)
+    ): View {
+        _binding = FragmentProductBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        switchCategory()
+        StateDataFactory(binding, productData, requireContext()).initStateCategory()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun switchCategory() {
+        binding.itemDetails.shopTv.setOnClickListener {
+            productData = ProductData.Shop
+            StateDataFactory(binding, productData, requireContext()).initStateCategory()
+        }
+        binding.itemDetails.detailsTv.setOnClickListener {
+            productData = ProductData.Details
+            StateDataFactory(binding, productData, requireContext()).initStateCategory()
+        }
+        binding.itemDetails.featuresTv.setOnClickListener {
+            productData = ProductData.Features
+            StateDataFactory(binding, productData, requireContext()).initStateCategory()
+        }
+    }
 }
