594cbdda57a79ccaeac3b9ffe142cb23662a8577
HayarovEd <edurd@rambler.ru>
2022-11-12 16:40:04 +0500

    release baidgs

--- app/src/main/java/com/edurda77/testeffectivmobile/MainActivity.kt
+++ app/src/main/java/com/edurda77/testeffectivmobile/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.edurda77.testeffectivmobile
 
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,15 +13,19 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.edurda77.mylibrary.navigation.AppNavigation
 import com.edurda77.testeffectivmobile.databinding.ActivityMainBinding
+import com.edurda77.testeffectivmobile.presentation.MainViewModel
+import com.edurda77.testeffectivmobile.utils.StateMainActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navView: BottomNavigationView
+    private val viewModel by viewModels<MainViewModel>()
     @Inject
     lateinit var coordinator: AppNavigation
 
@@ -35,9 +43,23 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.productFragment
             )
         )
-        var badge = navView.getOrCreateBadge(R.id.navigation_dashboard)
-        badge.isVisible = true
-        badge.number = 15
+        val badge = navView.getOrCreateBadge(R.id.navigation_dashboard)
+        viewModel.shopData.observe(this){
+            when (it) {
+                is StateMainActivity.Success -> {
+                    if (it.data.basket.isEmpty()) {
+                        badge.isVisible = false
+                    } else {
+                        badge.isVisible = true
+                        badge.number = it.data.basket.size
+                    }
+                }
+                is StateMainActivity.Error -> {
+                    Toast.makeText(this, it.error.message, Toast.LENGTH_LONG).show()
+                }
+                is StateMainActivity.Loading -> {}
+            }
+        }
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
@@ -55,6 +77,8 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+        //
+    // setSizeHomeIcon()
     }
 
     private fun showBottomNav() {
@@ -63,5 +87,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun hideBottomNav() {
         navView.visibility = View.GONE
+    }
+
+    private fun setSizeHomeIcon() {
+        val iconView = navView.getChildAt(0)
+        val displayMetrics = resources.displayMetrics
+        val layoutParams : ViewGroup.LayoutParams = iconView.layoutParams
+        layoutParams.height =
+            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 45f, displayMetrics).toInt()
+        layoutParams.width =
+            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 45f, displayMetrics).toInt()
+        iconView.layoutParams = layoutParams
     }
 }
--- app/src/main/java/com/edurda77/testeffectivmobile/presentation/MainViewModel.kt
+++ app/src/main/java/com/edurda77/testeffectivmobile/presentation/MainViewModel.kt
@@ -1,7 +1,37 @@
 package com.edurda77.testeffectivmobile.presentation
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.edurda77.cart.utils.StateCart
+import com.edurda77.mylibrary.network.NetworkRepository
+import com.edurda77.mylibrary.network.ResultNetwork
+import com.edurda77.testeffectivmobile.utils.StateMainActivity
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel (){
+class MainViewModel @Inject constructor(private val repository: NetworkRepository) : ViewModel() {
+    private val _shopData = MutableLiveData<StateMainActivity>(StateMainActivity.Loading)
+    val shopData = _shopData
+
+    init {
+        getDataFromRepo()
+    }
+
+    private fun getDataFromRepo() {
+        viewModelScope.launch {
+            when (val repoData = repository.getResponseBasket()) {
+                is ResultNetwork.Success -> {}
+                is ResultNetwork.SuccessItem -> {}
+                is ResultNetwork.SuccessBasket -> {
+                    _shopData.value = StateMainActivity.Success(repoData.data)
+                }
+                is ResultNetwork.Error -> {
+                    _shopData.value = StateMainActivity.Error(repoData.error)
+                }
+            }
+        }
+    }
 }
